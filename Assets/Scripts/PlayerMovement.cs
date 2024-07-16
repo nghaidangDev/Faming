a3a652841819bfeeb47947bf332f2c04fc9146ed
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-07-16 15:08:24 +0700

    feat: camera follow player, bound camera and tilemap collider

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     private float horizontal;
     private float vertical;
+    private bool isFacingRight;
 
     private Animator anim;
     private Rigidbody2D rb;
@@ -20,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         PlayerMove();
+        Flip();
     }
 
     private void PlayerMove()
@@ -29,5 +31,16 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(horizontal * speed, vertical * speed);
         anim.SetBool("run", (horizontal != 0 || vertical != 0));
+    }
+
+    private void Flip()
+    {
+        if ((isFacingRight && horizontal > 0) || (!isFacingRight && horizontal < 0))
+        {
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
     }
 }
