1538572573653c967e075e2c22ab18179898e5b5
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-07-18 22:14:41 +0700

    feat: chat with NPC

--- Assets/Scripts/NPC/NPCClickHandler.cs
+++ Assets/Scripts/NPC/NPCClickHandler.cs
@@ -1,12 +1,22 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NPCClickHandler : MonoBehaviour
 {
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public float wordSpeed;
+    public bool playerIsClose;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && playerIsClose)
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
@@ -20,6 +30,62 @@ public class NPCClickHandler : MonoBehaviour
 
     private void OnNPCClicked()
     {
-        Debug.Log("NPC clicked");
+        if (dialoguePanel.activeInHierarchy)
+        {
+            zeroText();
+        }
+        else
+        {
+            dialoguePanel.SetActive(true);
+            StartCoroutine(Typing());
+        }
+        Debug.Log("OK");
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index < dialogue.Length - 1) 
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
     }
 }
