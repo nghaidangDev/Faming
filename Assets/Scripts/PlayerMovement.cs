b53d5715c754677833523fa3cceacb4e716437d0
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-07-21 19:36:34 +0700

    feat: skill dash ability for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,15 +12,27 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
 
+    //player dash ability
+    private float activeMoveSpeed;
+    [SerializeField] private float dashSpeed;
+
+    [SerializeField] private float dashLength = .5f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        activeMoveSpeed = speed;
     }
 
     private void Update()
     {
         PlayerMove();
+        PlayerDashAbility();
         Flip();
     }
 
@@ -29,8 +41,37 @@ public class PlayerMovement : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        rb.velocity = new Vector2(horizontal * activeMoveSpeed, vertical * activeMoveSpeed);
         anim.SetBool("run", (horizontal != 0 || vertical != 0));
+    }
+
+    private void PlayerDashAbility()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+                anim.SetTrigger("dash");
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = speed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 
     private void Flip()
